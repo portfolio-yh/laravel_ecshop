fbb1f46812fef458f947373397d6df9daef5f09c
portfolio-yh <a411684a@yahoo.co.jp>
2019-12-08 04:11:02 +0900

    サイドバー部品化

--- resources/views/admin/sidevar.blade.php
+++ resources/views/admin/sidevar.blade.php
@@ -1,31 +1,19 @@
-    <div class="container-fluid">
-      <div class="row">
-        <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
-          <ul class="nav nav-pills flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"></a>
-            </li>
-          </ul>
-
-          <ul class="nav nav-pills flex-column">
-            <li class="nav-item">
-              <a class="nav-link" href="#">商品一覧(作成中)</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">商品登録(作成中)</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('category.index') }}">カテゴリ管理</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">受注管理(作成中)</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">会員管理(作成中)</a>
-            </li>
-          </ul>
-
-        </nav>
+<div class="py-4">
+    <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="#">商品一覧(作成中)</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">商品登録(作成中)</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{-- route('category.index') --}}">カテゴリ管理</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">受注管理(作成中)</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('member.index') }}">メンバー管理</a>
+        </li>
+    </ul>
+</div>
