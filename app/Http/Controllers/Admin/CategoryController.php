06751400f57e24957a0afdfbfbdae081c853297a
portfolio-yh <a411684a@yahoo.co.jp>
2020-01-30 10:01:18 +0900

    カラムの並び替え機能実装完了

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Admin;
+use DemeterChain\C;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 //use App\Http\Requests\Admin\CategoryFormRequest;
 use App\Categories;
+//use App\Admin;
+
+use Illuminate\Contracts\Validation\Validator;  // 追加
+use Illuminate\Http\Exceptions\HttpResponseException;  // 追加
+
+use Doctrine\DBAL\Driver\PDOException;
+use PDO;
+
+
+
 
 class CategoryController extends Controller
 {
@@ -20,17 +33,21 @@ class CategoryController extends Controller
         return view('admin.category.index', compact('categories'));
     }
 
+
+
     /**
      * @param $id
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show(Request $request, $id)
     {
-        $find_categories = new Categories();
-        $find_categories = Categories::find($id, ['id','parent_category_id','category_name']);
-        $child_categories = $find_categories->children()->get(['id','parent_category_id','category_name']);
-        return view('admin.category.show', compact('find_categories', 'child_categories'));
+        $current_categories = new Categories();
+        $current_categories = Categories::find($id, ['id','parent_category_id','category_name','sort_no','hierarchy']);
+        $child_categories = $current_categories->children()->get(['id','parent_category_id','category_name','sort_no','hierarchy']);
+        return view('admin.category.show', compact('current_categories', 'child_categories'));
     }
+
+
 
 
     /**
@@ -39,6 +56,18 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
+
+        $this->validator($request->all());
+
+        Categories::create([
+            'parent_category_id' => $request->get('parent_category_id'),
+            'category_name'      => $request->get('category_name'),
+            'hierarchy'          => $request->get('hierarchy'),
+            'creator_id'         => \Auth::user()->getAuthIdentifier(),
+            'sort_no'            => Categories::max('sort_no') + 1,
+            //'sort_no'          => \DB::table('t_categories')->max('sort_no') + 1,
+        ]);
+
         return back();
     }
 
@@ -75,4 +104,119 @@ class CategoryController extends Controller
     {
         //
     }
+
+
+    public function sortUp(Request $request)
+    {
+        $this->validator($request->all());
+
+        $sort_no = $request->query('sort_no');
+        $hierarchy = $request->query('hierarchy');
+        try {
+            $dbh = \DB::connection()->getPdo();
+            $stmt = $dbh->prepare("
+                UPDATE t_categories
+                SET
+                  sort_no = CASE
+                    WHEN sort_no = :sort_no
+                      THEN ( SELECT sort_no FROM t_categories WHERE sort_no < :sort_no AND hierarchy = :hierarchy  ORDER BY sort_no DESC limit 1 )
+                    WHEN sort_no = ( SELECT sort_no FROM t_categories WHERE sort_no < :sort_no AND hierarchy = :hierarchy ORDER BY sort_no DESC limit 1 )
+                      THEN :sort_no
+                    ELSE sort_no
+                    END
+                WHERE
+                  hierarchy =  :hierarchy
+            ");
+            $stmt->bindParam(':sort_no', $sort_no);
+            $stmt->bindParam('hierarchy', $hierarchy);
+            $stmt->execute();
+        } catch (\Exception $e) {
+            dd($e->getMessage()." - ".$e->getLine().PHP_EOL);
+        }
+
+        return back();
+
+    }
+
+
+    public function sortDown(Request $request){
+
+        $this->validator($request->all());
+
+        $sort_no = $request->query('sort_no');
+        $hierarchy = $request->query('hierarchy');
+        try {
+            $dbh = \DB::connection()->getPdo();
+            $stmt = $dbh->prepare("
+                UPDATE t_categories
+                SET
+                  sort_no = CASE
+                    WHEN sort_no = :sort_no
+                      THEN ( SELECT sort_no FROM t_categories WHERE sort_no > :sort_no AND hierarchy = :hierarchy  ORDER BY sort_no ASC limit 1 )
+                    WHEN sort_no = ( SELECT sort_no FROM t_categories WHERE sort_no > :sort_no AND hierarchy = :hierarchy ORDER BY sort_no ASC limit 1 )
+                      THEN :sort_no
+                    ELSE sort_no
+                    END
+                WHERE
+                  hierarchy =  :hierarchy
+            ");
+            $stmt->bindParam(':sort_no', $sort_no);
+            $stmt->bindParam('hierarchy', $hierarchy);
+            $stmt->execute();
+        } catch (\Exception $e) {
+            dd($e->getMessage()." - ".$e->getLine().PHP_EOL);
+        }
+
+        return back();
+
+
+    }
+
+
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return array
+     */
+    protected function validator(array $data)
+    {
+        return \Validator::make($data, [
+            'category_name' => ['filled','string','unique:t_categories','max:50'],
+            'hierarchy' => ['required','integer'],
+            'parent_category_id' => ['nullable', 'integer', 'min:1'],
+            'sort_no' => ['filled', 'integer'],
+        ])->validate();
+
+
+//        $validator = \Validator::make($data, [
+//            'category_name' => ['required','string','unique:t_categories','max:50'],
+//            'hierarchy' => ['required','integer'],
+//            'parent_category_id' => ['nullable', 'integer', 'min:1']
+//        ]);
+
+//        $response['data']    = [];
+//        $response['status']  = 'NG';
+//        $response['summary'] = 'Failed validation.';
+//        $response['errors']  = $validator->errors()->toArray();
+//
+//        throw new HttpResponseException(
+//            response()->json( $response, 422 )
+//        );
+
+//        return response()->json([
+//            'name' => ‘taro’,
+//            'id' => 1
+//        ]);
+
+//        if ($validator->fails()) {
+//            return back()
+//                ->withErrors($validator)
+//                ->withInput();
+//        }
+
+    }
+
+
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,21 +1,15 @@
 @extends('layouts.admin')
 @section('content')
-    <form action="{{ route( 'category.store') }}" method="post">
-        <div class="form-row mb-3">
-            <div class="col-auto">
-                {{ csrf_field() }}
-                <input type="hidden" id="parent_category_id" name="parent_category_id" value="1">
-                <input type="hidden" id="hierarchy" name="hierarchy" value="1">
-            </div>
-            <div class="col-auto">
-                <span>@error('category_name')※@enderror</span>
-            </div>
-            <div class="col-auto">
-                <input type="text" id="category_name" name="category_name" class="form-control" value="{{ old('category_name') }}" required="required">
-            </div>
-            <div class="col-auto">
-                <input class="btn btn-primary" type="submit" value="カテゴリ追加">
-            </div>
+    <form action="{{ route( 'category.store') }}" method="post" class="form-inline">
+        @csrf
+        @method('POST')
+        <div class="form-group mb-3">
+            <span>@error('category_name')※@enderror</span>
+            <input type="text" id="category_name" name="category_name" class="form-control"
+                   value="{{ old('category_name') }}" required="required">
+            {{--            <input type="hidden" id="parent_category_id" name="parent_category_id" value="">--}}
+            <input type="hidden" id="hierarchy" name="hierarchy" value="0">
+            <input class="btn btn-primary ml-1" type="submit" value="この階層にカテゴリ追加">
         </div>
     </form>
 
@@ -23,6 +17,10 @@
         <thead>
         <tr>
             <th>カテゴリ</th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
         </tr>
         </thead>
         <tbody>
@@ -32,26 +30,20 @@
                     <a href="{{ route( 'category.show', ['category' => $category->id] ) }}">{{ $category->category_name }}</a>
                 </td>
                 <td>
+                    <a href="{{ url('/admin/category_sortUp/?sort_no=' . $category->sort_no . '&hierarchy='  . $category->hierarchy .'') }}" class="btn btn-warning">↑</a>
+                </td>
+                <td>
+                    <a href="{{ url('/admin/category_sortDown/?sort_no=' . $category->sort_no . '&hierarchy='  . $category->hierarchy .'') }}" class="btn btn-warning">↓</a>
+                </td>
+                <td>
                     <form action="{{ route( 'category.edit', ['category' => $category->id] ) }}" method="post">
-                        {{ csrf_field() }}
-                        <input class="btn btn-warning" type="submit" value="⇧">
+                        @csrf
+                        <input class="btn btn-success" type="button" value="編集">
                     </form>
                 </td>
                 <td>
                     <form action="{{ route( 'category.edit', ['category' => $category->id] ) }}" method="post">
-                        {{ csrf_field() }}
-                        <input class="btn btn-warning" type="submit" value="⇩">
-                    </form>
-                </td>
-                <td>
-                    <form action="{{ route( 'category.edit', ['category' => $category->id] ) }}" method="post">
-                        {{ csrf_field() }}
-                        <input class="btn btn-success" type="submit" value="編集">
-                    </form>
-                </td>
-                <td>
-                    <form action="{{ route( 'category.edit', ['category' => $category->id] ) }}" method="post">
-                        {{ csrf_field() }}
+                        @csrf
                         <input class="btn btn-danger" type="submit" value="削除">
                     </form>
                 </td>
